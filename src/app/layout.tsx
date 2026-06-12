acdb8f97d94ce1d28eae1c47a929f28b7a35795f
Joshua Eaton <UnimatrixZeroRD@proton.me>
2026-06-11 22:37:25 -0300

    Apply page background class

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import { SiteFooter } from "../components/site-footer";
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body>
+      <body className="quiet-page-bg">
         <SiteHeader />
         {children}
         <SiteFooter />
