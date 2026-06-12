48131deb86fff99f72d7304e5ca0abcc8356aabb
Joshua Eaton <UnimatrixZeroRD@proton.me>
2026-06-11 21:25:09 -0300

    Wire header and footer into root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,15 @@
 import "./globals.css";
+import { SiteHeader } from "../components/site-header";
+import { SiteFooter } from "../components/site-footer";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <SiteHeader />
+        {children}
+        <SiteFooter />
+      </body>
     </html>
   );
 }
