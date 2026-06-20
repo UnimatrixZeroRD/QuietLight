982a278e4fe6131363bf41a2627eea9258a3545b
Joshua Eaton <UnimatrixZeroRD@proton.me>
2026-06-20 02:50:20 -0300

    Add dynamic XML sitemap route

---
+++ src/app/sitemap.xml/route.ts
@@ -0,0 +1,15 @@
+import { buildSitemapXml } from "../../lib/site-seo";
+
+export const dynamic = "force-dynamic";
+export const revalidate = 300;
+
+export async function GET() {
+  const xml = await buildSitemapXml();
+
+  return new Response(xml, {
+    headers: {
+      "Content-Type": "application/xml; charset=utf-8",
+      "Cache-Control": "public, s-maxage=300, stale-while-revalidate=86400",
+    },
+  });
+}
